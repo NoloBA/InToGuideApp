bedac47ceb4d7af2f897b7b2e98804838728d1eb
Ashley <4270837@myuwc.ac.za>
2022-07-05 14:57:59 +0200

    .

--- InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
+++ InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
@@ -19,7 +19,7 @@ namespace InToGuideApp
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
