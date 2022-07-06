9dc465ea87ab804388e6f2d4b830575fb132737f
4270878 <4270878@myuwc.ac.za>
2022-07-06 13:23:18 +0200

    inter

--- InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
+++ InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
@@ -19,7 +19,7 @@ namespace InToGuideApp
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync("NavigationPage/HelpAndSupportPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
