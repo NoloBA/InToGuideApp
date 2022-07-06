58e77878623185066a9d6bd13121c842d828393e
LaurenArendse <102674344+LaurenArendse@users.noreply.github.com>
2022-07-06 11:47:35 +0200

    Added authentication model table.

--- InToGuideAppSln/InToGuideWebAPI/Models/Authentication.cs
+++ InToGuideAppSln/InToGuideWebAPI/Models/Authentication.cs
@@ -6,5 +6,13 @@ namespace InToGuideWebAPI.Models
     [Table("Authentication")]
     public class Authentication
     {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int AuthenticationId { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public bool VerificationStatus { get; set; }
     }
 }
