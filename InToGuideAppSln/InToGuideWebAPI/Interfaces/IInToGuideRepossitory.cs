5c0ddc49a2fecad0cc2d0773f716fa1b8b42edbb
BONOLO ATONG <bonoloatong@gmail.com>
2022-07-13 11:10:08 +0200

    Interfaces test commit

--- InToGuideAppSln/InToGuideWebAPI/Interfaces/IInToGuideRepossitory.cs
+++ InToGuideAppSln/InToGuideWebAPI/Interfaces/IInToGuideRepossitory.cs
@@ -1,5 +1,4 @@
 ﻿
-using InToGuideWebAPI.Controllers;
 using InToGuideWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,30 +8,21 @@ namespace InToGuideWebAPI.Interfaces
     {
         Certificate CreateNewCertificate(Certificate certificate);
         User CreateNewUser(User user);
-        User GetUserByUserId(int UserId, bool fullFetch = true);
         Chat CreateChat(Chat chat);
         bool DoesThreadIdExistByUser(string threadId);
         bool DoesMessageExistByUser(string message);
-        List<Chat> GetChatList(int ChatId,bool fullFetch = true);
-        Chat GetChat(string chatId);
+        List<Chat> GetChatList(bool fullFetch = true);
 
         HelpAndSupport CreateNewHelpAndSupport(HelpAndSupport helpAndSupport);
         bool DoesEmailAddresExistByUser(string email);
         List<HelpAndSupport> GetHelpAndSupports(bool fullFetch = true);
-        IActionResult CreateNewMatch();
         MentorHistory CreateNewMentorHistory(MentorHistory mentorhistory);
-        IEnumerable<Match> GetAllMatches();
-        MentorHistory GetMentorHistory(int HistorId, bool fullFetch = true);//!!
-        List<MentorHistory> GetMentorHistorys(bool fullFetch = true);
-
-
+        IActionResult CreateNewMatch();
         bool DoesHistoryIdExistUser(MentorHistory mentorhistory);
-        Review CreateNewReview(Review review);
-        List<Review> GetAllReviews(bool fullFetch = true);
-        Task<object?> CreateNewUser();
-        bool PerformAuthenticationCheck(string userName, string password);
-        IEnumerable<Chat> GetChatList();
-        Match CreateNewMatch(Match match);
-        List<Match> GetAllMatches(int MatchId,bool fullFetch = true);
+        Review  CreateNewReview(Review review);
+
+
+
+
     }
 }
