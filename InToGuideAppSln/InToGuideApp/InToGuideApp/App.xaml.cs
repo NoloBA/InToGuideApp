f43be33b86f22b1b02fd356db9a74164ab54a2cc
4270878 <4270878@myuwc.ac.za>
2022-07-13 13:34:20 +0200

    pullings

--- InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
+++ InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
@@ -19,7 +19,7 @@ namespace InToGuideApp
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MentorTabbedPage");
+            await NavigationService.NavigateAsync("NavigationPage/MentorProfilePage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
