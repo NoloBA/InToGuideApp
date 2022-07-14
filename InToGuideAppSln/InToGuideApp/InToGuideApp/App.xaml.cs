19f0547296fcbd7c0c1f61a57b1d5b93f7bf795b
LaurenArendse <102674344+LaurenArendse@users.noreply.github.com>
2022-07-14 12:55:48 +0200

    Fixed Navifation on Mentor Create Accont Page to navigate to Login Page.

--- InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
+++ InToGuideAppSln/InToGuideApp/InToGuideApp/App.xaml.cs
@@ -19,7 +19,7 @@ namespace InToGuideApp
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MentorProfileView");
+            await NavigationService.NavigateAsync("NavigationPage/MenteeCreateAccountPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
