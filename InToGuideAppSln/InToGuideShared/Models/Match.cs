14be24993eca41f18c635566aab2cd741b8f91b3
LaurenArendse <102674344+LaurenArendse@users.noreply.github.com>
2022-07-18 15:47:46 +0200

    Moved all models to shared project

---
+++ InToGuideAppSln/InToGuideShared/Models/Match.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InToGuideWebAPI.Models
+{
+    [Table("Match")]
+    public class Match
+    {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int MatchId { get; set; }
+        public int MenteeId { get; set; }
+        public int MentorId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        //public  User? User { get; set; }
+
+
+        
+
+
+
+    }
+}
